2c8d7c91239cb01cd014eeac414f81c423dd79e2
Roger Fillion <rogerfillion@gmail.com>
2021-08-14 16:44:44 -0400

    Ajoutez des fichiers projet.

---
+++ SolutionSherWeb/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SolutionSherWeb
+{
+    public class Solution
+    {
+        public int mySolution(int[] A, int K)
+        {
+            var myList = A.Combinations(K).ToList()
+                .Select(i => i.ToList()).ToList()
+                .Where(x => (x.Sum() + 2) % 2 == 0);
+            
+            if (myList.Count() == 0) return -1;
+
+            var result=myList
+                    .Max(x=> x.Sum());
+            return result;
+        }
+    }
+}
---
+++ SolutionSherWebTests/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolutionSherWeb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace SolutionSherWeb.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        [DataRow(18, 3, 4, 9, 8, 2, 6)]
+        [DataRow(20, 5, 5, 6, 3, 4, 2)]
+        [DataRow(-1, 1, 7, 7, 7, 7, 7)]
+        [DataRow(-1, 1000, 2)]
+        [DataRow(12, 3, 2, 3, 3, 5, 5)]
+        public void mySolutionTest(int r, int K, params int[] data)
+        {
+            var o = new Solution();
+
+            var result = o.mySolution(data, K);
+
+            result.Should().Be(r);
+        }
+    }
+}
